d86c114feed7aa8352f21ccf72b345e9f04bf330
f-elix <guerinfelix08@gmail.com>
2025-05-03 10:51:59 -0400

    Implement dashboard layout (task 2)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,17 +8,15 @@
 </script>
 
 <div class="flex h-screen flex-col">
-	<header
-		class="flex items-center justify-between gap-4 border-b border-gray-50/20 bg-gray-800 p-4"
-	>
-		<h1 class="text-xl font-bold">Felix Guerin - Take home</h1>
+	<header class="flex items-center justify-between gap-4 border-b border-gray-700 bg-gray-800 p-4">
+		<h1 class="font-medium">Felix Guerin - Take home</h1>
 		{#if user}
 			<form action="/api/auth/logout" method="POST">
 				<button>Logout</button>
 			</form>
 		{/if}
 	</header>
-	<main class="grow p-4">
+	<main class="grow">
 		{@render children?.()}
 	</main>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,16 @@
 <script lang="ts">
-	let { data } = $props();
 </script>
 
-<pre>{JSON.stringify(data, null, 4)}</pre>
+<div class="h-full bg-gray-700">
+	<div class="grid h-full grid-rows-5 gap-[1px] md:grid-cols-5">
+		<div class="row-span-2 bg-gray-900 p-4 md:col-span-2 md:row-span-full">
+			<h2 class="text-lg font-bold">Search</h2>
+		</div>
+		<div class="bg-gray-900 p-4 md:col-span-3 md:row-span-2">
+			<h2 class="text-lg font-bold">Profile</h2>
+		</div>
+		<div class="row-span-2 bg-gray-900 p-4 md:col-span-3 md:col-start-3 md:row-span-3">
+			<h2 class="text-lg font-bold">My friends</h2>
+		</div>
+	</div>
+</div>
